9e4df5d00014fcb8d8e8671beec00ff1010bc46c
profmanjupriya <mmuralidharanpriya@sdsu.edu>
2025-11-25 21:19:25 -0800

    Logo Added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React from 'react'
+import logo from './logo2.png'
 export default function Hero() {
   return (
-    <section id="hero" className="min-h-[60vh] flex flex-col justify-center items-center bg-boltsRed text-boltsWhite">
+    <section
+      id="hero"
+      className="min-h-[60vh] flex flex-col justify-center items-center bg-boltsRed text-boltsWhite"
+    >
       {/* Placeholder logo */}
-      <div className="w-28 h-28 bg-boltsWhite rounded-full flex items-center justify-center mb-6">
-        <span className="text-boltsRed font-black text-3xl">B</span>
+      <div className="mb-6">
+        <img
+          src={logo}
+          alt="Bolts Robotics Logo"
+          className="w-28 h-28 object-contain"
+        />
       </div>
-      <h1 className="text-5xl font-extrabold mb-4 drop-shadow-lg">Bolts</h1>
-      <h2 className="text-lg font-medium">College VEX U Robotics Team</h2>
+      <h2 className="text-lg font-medium">VEX U Robotics Team</h2>
     </section>
-  );
+  )
 }
